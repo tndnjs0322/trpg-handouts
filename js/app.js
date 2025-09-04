a010fb67ae89f5116e94eaa74632260342cd385c
SRag <tndnjs0322@gmail.com>
2025-09-04 12:19:33 +0900

    Delete js directory

--- js/app.js
+++
@@ -1,8 +0,0 @@
-// 커서 깜박임 제거용
-document.addEventListener("DOMContentLoaded", () => {
-  setTimeout(() => {
-    document.querySelectorAll('.handout__title').forEach(h => {
-      h.classList.add('caret-hide');
-    });
-  }, 2500);
-});
