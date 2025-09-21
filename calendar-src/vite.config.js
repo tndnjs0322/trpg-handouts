ac7e0f7f2431ffc12d58fa1464c7b35b05c2de13
SRag <tndnjs0322@gmail.com>
2025-09-21 13:37:57 +0900

    Update vite.config.js

--- calendar-src/vite.config.js
+++ calendar-src/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
-
 export default defineConfig({
-  base: './',                 // 상대 경로 (어디에 배포해도 안전)
+  base: './',
   build: {
-    outDir: '../schedule',    // ✅ 레포 루트의 /schedule 폴더로 산출
-    emptyOutDir: true
+    outDir: '../schedule',   // ✅ 이미 있는 폴더 이름
+    emptyOutDir: true        // ✅ 빌드시 폴더 내부 비움(자동 정리)
   }
 });
