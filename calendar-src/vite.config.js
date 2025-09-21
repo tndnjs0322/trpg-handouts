74030d05e1a23c9dae0285b8fbb55a4dd4605abe
SRag <tndnjs0322@gmail.com>
2025-09-21 15:25:29 +0900

    Delete calendar-src directory

--- calendar-src/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: './',                 // ✅ 상대 경로 (GitHub Pages에서 경로 깨짐 방지)
-  build: {
-    outDir: '../schedule',    // ✅ 빌드 결과를 레포 루트의 /schedule 로 내보내기
-    emptyOutDir: true         // ✅ 빌드할 때 /schedule 내부 정리 후 교체
-  }
-});
